7d1277b623a1d636392d2b1a1f5b096ee204ee71
RossiAlex <financeiro.alexsander@gmail.com>
2020-04-17 04:14:35 -0300

    instal pipes

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -1,35 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.services';
 
 @Component ({
     selector: 'app-course-List',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
-    courses: Course[] = [];
-    ngOnInit(): void {
-        this.courses = [
-        {
-            id: 1,
-            name: 'Angular: Forms',
-            imageUrl: '/assets/image/forms.png',
-            price: 99.99,
-            code: 'XPS-8796',
-            duration: 120,
-            rating: 4.5,
-            releaseDate: 'April, 15, 2020'
-        },
-        {
-            id: 2,
-            name: 'Angular: HTTP',
-            imageUrl: '/assets/image/http.png',
-            price: 45.99,
-            code: 'LKL-1094',
-            duration: 80,
-            rating: 4,
-            releaseDate: 'April, 15, 2020'
-        }
-    ]
+    
+    filteredCourses: Course[] = [];
+    
+    _courses: Course[] = [];
+
+    _filterBy: string;
+
+    constructor(private courseService: CourseService) { }
+
+    ngOnInit(): void {     
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string) {
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) =>
+        course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter() {
+        return this._filterBy;
     }
 
 }
