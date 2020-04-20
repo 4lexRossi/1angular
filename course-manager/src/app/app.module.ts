05d0a4c0f4ae3e09d494ff6dddd8f0861c27c65d
RossiAlex <financeiro.alexsander@gmail.com>
2020-04-20 17:06:01 -0300

    Criação pasta Core e segregacoes

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -2,21 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,       
-    NavBarComponent,
+    AppComponent,    
     Error404Component    
   ],
   imports: [
     BrowserModule,    
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
